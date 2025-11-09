79e472322e0bcd5e5d9d28744fde4b286f189b90
thehando0714 <chrishu0714@gmail.com>
2025-11-09 22:37:01 +0800

    Implement kidsWithCandies function in Leetcode1431

---
+++ Study_Note/Array/Leetcode1431.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<int>vt=candies;
+        sort(vt.begin(),vt.end());
+        vector<bool>ans(candies.size());
+        for(int i=0;i<candies.size();i++){
+            if(candies[i]>=vt[vt.size()-1]||candies[i]+extraCandies>=vt[vt.size()-1]){
+                ans[i]=true;
+            }
+            else{
+                ans[i]=false;
+            }
+        }
+        return ans;
+    }
+};
