bcc98419c0d6ad006d4e3e134f2df0270c464503
thehando0714 <chrishu0714@gmail.com>
2025-11-09 22:31:48 +0800

    Implement pivot index solution in Leetcode724

---
+++ Study_Note/Array/Leetcode724.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        vector<int>pre(nums.size()+1);
+        pre[0]=0;
+        for(int i=0;i<nums.size();i++){
+            pre[i+1]=pre[i]+nums[i];
+        }
+        for(int i=1;i<pre.size();i++){
+            if(pre[pre.size()-1]-pre[i]==pre[i-1]){
+                return i-1;
+            }
+        }
+        return -1;
+    }
+};
